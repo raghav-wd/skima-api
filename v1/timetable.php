b7a1e517c3ca0ef724cfac6543f07dffb6bbe2c0
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-10-30 18:21:28 +0530

    Implement scraper class on timetable.php

--- v1/timetable.php
+++ v1/timetable.php
@@ -3,51 +3,30 @@ error_reporting(E_ERROR);
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
+include 'simple_html_dom.php';
+include './utils/Scraper.php';
+
 //Receiving content/keys in json format
 $creds = json_decode(file_get_contents("php://input")) ?: (object) array();
 
 $cookieKey = $creds->Cookie;
 $timetableKey = $creds->TimeTableKey;
 
-include 'simple_html_dom.php';
  
-//Upload a blank cookie.txt to the same directory as this file with a CHMOD/Permission to 777
-function login($url,$data, $header){
-    $fp = fopen("cookie.txt", "w");
-    fclose($fp);
-    $headers = $header;
-    $login = curl_init();
-    curl_setopt($login, CURLOPT_COOKIEJAR, "cookie.txt");
-    curl_setopt($login, CURLOPT_COOKIEFILE, "cookie.txt");
-    curl_setopt($login, CURLOPT_HEADER, TRUE);
-    curl_setopt($login, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($login, CURLOPT_TIMEOUT, 40000);
-    curl_setopt($login, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($login, CURLOPT_URL, $url);
-    curl_setopt($login, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-    curl_setopt($login, CURLOPT_FOLLOWLOCATION, TRUE);
-    curl_setopt($login, CURLOPT_POST, TRUE);
-    curl_setopt($login, CURLOPT_POSTFIELDS, $data);
-    ob_start();
-    return curl_exec ($login);
-    ob_end_clean();
-    curl_close ($login);
-    unset($login);    
-}
-
 //1
 $loginURL = "https://academia.srmist.edu.in/liveViewHeader.do";
 $data = "sharedBy=srm_university&appLinkName=academia-academic-services&zccpn=bts&viewLinkName=".$timetableKey."&&urlParams=%7B%7D&isPageLoad=true";
 $headers = array("Cookie: ".$cookieKey);
 
-$html = login($loginURL, $data, $headers);
+$scraper = new Scraper($loginURL, $data, $headers)
+$html = $scraper->login();
+
 $html = preg_replace_callback('/\\\\x([0-9A-F]{1,2})/i', function ($m) {
     return chr(hexdec($m[1]));
 }, $html);
 $html = substr($html, strpos($html, '<div class="mainDiv">'));
 $html = str_get_html($html);
-echo $html;
-// echo $html;
+
 $response = [];
 $Student_Details = [];
 $c = 0;
